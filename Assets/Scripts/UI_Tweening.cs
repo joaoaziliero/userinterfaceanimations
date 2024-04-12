a320a9dba6db3b9260c87ce01940f5c32923c0cb
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-04-12 15:01:22 -0300

    Update UI_Tweening.cs

--- Assets/Scripts/UI_Tweening.cs
+++ Assets/Scripts/UI_Tweening.cs
@@ -1,18 +1,35 @@
+using NaughtyAttributes;
+using DG.Tweening;
+using R3;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using DG.Tweening;
 using System.Linq;
+using UnityEngine;
 
 public class UI_Tweening : MonoBehaviour
 {
     [SerializeField] private List<Transform> _icons;
+    [SerializeField] private float _scale;
     [SerializeField] private float _animationDuration;
-    [SerializeField] private float _animationIntervalSize;
+    [SerializeField] private float _animationDelay;
 
-    //private List<Observable<Unit>> SetAnimations(List<Transform> icons, float interval, float duration)
-    //{
-    //    return icons.Select((obj, j) => Observable.Timer(TimeSpan.FromSeconds(j * interval)).Do(_ => obj.DOScale(0, duration).From()))
-    //        .ToList();
-    //}
+    [Button]
+    private void HideIcons()
+    {
+        SetAnimations(_icons, 0, _animationDelay, _animationDuration).ForEach(obs => obs.Subscribe());
+    }
+
+    [Button]
+    private void ShowIcons()
+    {
+        SetAnimations(_icons, 1, _animationDelay, _animationDuration).ForEach(obs => obs.Subscribe());
+    }
+
+    private List<Observable<Unit>> SetAnimations(List<Transform> icons, float scale, float delay, float duration)
+    {
+        return icons
+            .Select((obj, j) => Observable.Timer(TimeSpan.FromSeconds(j * delay)).Do(_ => obj.DOScale(scale, duration)))
+            .ToList();
+    }
 }
